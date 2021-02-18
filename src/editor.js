14e430849b7c3b042fdb2c50b962a27ecad80bda
Dave Bauer <dave.bauer@gmail.com>
2021-02-18 12:27:41 -0500

    Add editor for standalone use

---
+++ src/editor.js
@@ -0,0 +1,45 @@
+import './h5p-integration';
+import H5PStandalone from './h5p-standalone.class';
+import 'H5PEventDispatcher';
+import 'H5PxAPI';
+import 'H5PxAPIEvent';
+import 'H5PContentType';
+
+import 'h5p-editor/h5p-hub-client';
+
+import 'h5p-editor/h5p-hub-client.js';
+import 'h5p-editor/h5peditor.js';
+import 'h5p-editor/h5peditor-semantic-structure.js';
+import 'h5p-editor/h5peditor-editor.js';
+import 'h5p-editor/h5peditor-library-selector.js';
+import 'h5p-editor/h5peditor-fullscreen-bar.js';
+import 'h5p-editor/h5peditor-form.js';
+import 'h5p-editor/h5peditor-text.js';
+import 'h5p-editor/h5peditor-html.js';
+import 'h5p-editor/h5peditor-number.js';
+import 'h5p-editor/h5peditor-textarea.js';
+import 'h5p-editor/h5peditor-file-uploader.js';
+import 'h5p-editor/h5peditor-file.js';
+import 'h5p-editor/h5peditor-image.js';
+import 'h5p-editor/h5peditor-image-popup.js';
+import 'h5p-editor/h5peditor-av.js';
+import 'h5p-editor/h5peditor-group.js';
+import 'h5p-editor/h5peditor-boolean.js';
+import 'h5p-editor/h5peditor-list.js';
+import 'h5p-editor/h5peditor-list-editor.js';
+import 'h5p-editor/h5peditor-library.js';
+import 'h5p-editor/h5peditor-library-list-cache.js';
+import 'h5p-editor/h5peditor-select.js';
+import 'h5p-editor/h5peditor-selector-hub.js';
+import 'h5p-editor/h5peditor-selector-legacy.js';
+import 'h5p-editor/h5peditor-dimensions.js';
+import 'h5p-editor/h5peditor-coordinates.js';
+import 'h5p-editor/h5peditor-none.js';
+import 'h5p-editor/h5peditor-metadata.js';
+import 'h5p-editor/h5peditor-metadata-author-widget.js';
+import 'h5p-editor/h5peditor-metadata-changelog-widget.js';
+import 'h5p-editor/h5peditor-pre-save.js';
+import 'ckeditor/ckeditor.js'
+
+// Then load the scripts/h5peditor-init.js which sets up the editor object
+// see h5p-standalone.js we probably need an h5p-editor-standalone.js also
