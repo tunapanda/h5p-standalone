d1dbdb154f0f76df78d739ff2ff03ce51033e8df
murage <murage@labskenya.com>
2021-11-01 18:08:10 +0300

    test(e2e): refactor basic multiple player h5p tests

--- cypress/integration/multiple.spec.js
+++ cypress/integration/multiple.spec.js
@@ -1,14 +1,30 @@
-describe('multiple', () => {
-  it('should display 2 h5ps', () => {
+describe("Multiple H5P players", () => {
+  beforeEach(() => {
+    cy.visit("test/multiple.html");
+  });
 
-    cy.visit('test/multiple.html');
+  it("should display & load multiple h5p players", () => {
+    cy.iframe("#h5p-container-1 iframe.h5p-iframe.h5p-iframe.h5p-initialized")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".h5p-true-false-answers .h5p-true-false-answer")
+          .contains("False")
+          .click();
 
-    cy.get('#h5p-container-1 .h5p-iframe').should(iframe => {
-      expect(iframe.contents().find('.h5p-content')).to.exist;
-    });
+        cy.get(".h5p-question-check-answer").click();
 
-    cy.get('#h5p-container-2 .h5p-iframe').should(iframe => {
-      expect(iframe.contents().find('.h5p-content')).to.exist;
-    });
+        cy.get(".h5p-joubelui-score-bar-star").should("be.visible");
+      });
+    cy.iframe("#h5p-container-2  iframe.h5p-iframe.h5p-iframe.h5p-initialized")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".h5p-true-false-answers .h5p-true-false-answer")
+          .contains("False")
+          .click();
+
+        cy.get(".h5p-question-check-answer").click();
+
+        cy.get(".h5p-joubelui-score-bar-star").should("be.visible");
+      });
   });
 });
