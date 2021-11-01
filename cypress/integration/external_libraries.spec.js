b45d3dbbaa371dc38d53081625abc67b7c2acb9d
murage <murage@labskenya.com>
2021-11-01 18:08:49 +0300

    test(e2e): refactor h5p player w/ external libraries tests

--- cypress/integration/external_libraries.spec.js
+++ cypress/integration/external_libraries.spec.js
@@ -1,16 +1,19 @@
-describe('external libraries', () => {
-  it('should display h5p', () => {
+describe("H5P player loading external libraries", () => {
+  beforeEach(() => {
+    cy.visit("test/external_libraries.html");
+  });
 
-    cy.visit('test/external_libraries.html');
+  it("should display h5p", () => {
+    cy.iframe("iframe.h5p-iframe.h5p-iframe.h5p-initialized")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".h5p-true-false-answers .h5p-true-false-answer")
+          .contains("False")
+          .click();
 
-    cy.get('.h5p-iframe').should(iframe => {
-      expect(iframe.contents().find('.h5p-content')).to.exist;
+        cy.get(".h5p-question-check-answer").click();
 
-      iframe.contents().find('.h5p-true-false-answer').click();
-
-      iframe.contents().find('.h5p-question-check-answer').click();
-
-      expect(iframe.contents().find('.h5p-joubelui-score-bar-star')).to.exist;
-    });
+        cy.get(".h5p-joubelui-score-bar-star").should("be.visible");
+      });
   });
 });
