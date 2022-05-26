7157eb58e44455e6f25d3bb70ec1b1ba6f51094d
murage <murage@labskenya.com>
2022-05-26 19:33:20 +0300

    refactor(fix): add a hack to prevent h5p prototype error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import {H5PStandalone} from "./h5p-standalone";
 
 
-export default {H5P: H5PStandalone}
+const H5P = H5PStandalone
+
+//todo: this is a hackish way to avoid prototype errors, rename H5P to Player or something else
+H5P['EventDispatcher'] = () => {
+}
+
+export default {H5P}
